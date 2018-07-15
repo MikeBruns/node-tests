0e2f2198d33c6e7f9d0ef7d2dba58d486ee4b31c
Michael Bruns <michaelbruns99@gmail.com>
2018-07-15 17:04:22 -0400

    testing different expect functions

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -11,3 +11,26 @@ it('should square a number', () => {
   var res = utils.square(3);
   expect(res).toBe(9).toBeA('number');
 });
+
+it('should expect some values', () => {
+  expect(12).toBe(12);
+  expect(12).toNotBe(11);
+
+  expect({name: 'Michael'}).toEqual({name: 'Michael'});
+  expect({name: 'michael'}).toNotEqual({name: 'Michael'});
+
+  expect([2,3,4]).toInclude(2);
+  expect([2,3,4]).toExclude(5);
+
+  expect({
+    name: 'Michael',
+    age: 24,
+    location: 'Cleveland'
+  }).toInclude({age: 24});
+  expect({
+    name: 'Michael',
+    age: 24,
+    location: 'Cleveland'
+  }).toExclude({age: 606});
+
+});
