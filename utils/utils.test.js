f339352664a596b45e1f10981df1b0e38c197659
Michael Bruns <michaelbruns99@gmail.com>
2018-07-15 16:54:59 -0400

    using expect to check test values

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,13 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
-  if (res !== 44) {
-    throw new Error(`Expected 44, got ${res}`);
-  }
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
   var res = utils.square(3);
-  if (res !== 9) {
-    throw new Error(`Expected 9, got ${res}`);
-  }
+  expect(res).toBe(9).toBeA('number');
 });
